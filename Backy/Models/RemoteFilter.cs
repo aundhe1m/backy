1e8ea29476db90828f2cca9e174f732d299bef61
Anders Undheim <andersund@gmail.com>
2024-11-04 19:19:42 +0100

    One step forward, still broken modals

--- Backy/Models/RemoteFilter.cs
+++ Backy/Models/RemoteFilter.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backy.Models
 {
     public class RemoteFilter
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
